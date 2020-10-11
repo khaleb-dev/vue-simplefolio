073b3958d9da8060851f90d824e508a7d955d6be
Khaleb O'Brien <ebukauche52@gmail.com>
2020-10-11 10:06:39 +0100

    installed bootstrap dependencies

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import App from './App.vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// import $ from 'jquery'
+import VueTilt from 'vue-tilt.js'
+import PortalVue from 'portal-vue'
+// import Popper from 'popper.js'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(VueTilt)
+Vue.use(PortalVue)
 
 new Vue({
   render: h => h(App),
